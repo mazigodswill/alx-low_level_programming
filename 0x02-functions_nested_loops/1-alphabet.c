9fe18e3184c7f90dd85b5c15d67b266c0653b8ed
mazigodswill <godswillnathaniel97@gmail.com>
2023-02-21 12:38:34 -0800

    alx is hard tho

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+
+		_putchar(x);
+
+	}
+	_putchar('\n');
+}
